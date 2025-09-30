8c76196b004fb440f84b2e975a676faacf23025a
protectflame <magaomaev@gmail.com>
2025-09-30 13:25:58 +0300

    Добавлен пример кода и настройка анализа. FIX

--- App.java
+++ App.java
@@ -2,7 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        String message = "Hello";
         message = "World";
         System.out.println(message);
     }
