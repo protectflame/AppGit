3838896d2df55036f01b4f8cb5b9ed68d084a4f5
protectflame <magaomaev@gmail.com>
2025-09-30 13:23:50 +0300

    Добавлен пример кода и настройка анализа

---
+++ App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        String message = "Hello";
+        message = "World";
+        System.out.println(message);
+    }
+}
